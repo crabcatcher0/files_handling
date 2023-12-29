9cfed0f6dd57b24ac554f79b04ddb4dcfe5d910a
crabcatcher0 <rautprashant103@gmail.com>
2023-12-29 19:04:09 +0545

    Simple file read

---
+++ main.py
@@ -0,0 +1,4 @@
+from .parse_file import toread
+
+if __name__ == "__main__":
+    toread()
--- parse_file.py
+++ parse_file.py
@@ -1,2 +1,17 @@
 import csv
 
+def toread(col_index):
+    print("*** Welcome ***")
+    print("Please Choose:\n[1].Customer Id\n[2].First Name\n[3].Last Name\n[4].Company\n[5].City\n[6].Country\n[7].Phone 1\n[8].Phone 2\n[9].Email\n[10].Subscription Date\n[11].Website")
+    col_index = int(input("Please choose the number from above."))
+
+    with open('customers.csv') as file_csv:
+        file = csv.reader(file_csv)
+
+        for line in file:
+            print(line[col_index])
+
+
+
+ram = toread(1)
+print(ram)
